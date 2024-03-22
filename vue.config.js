b592f845717db636a504c7469ba92968d149ba51
Peter Iskander <petermagdyiskander@gmail.com>
2024-03-22 21:03:17 +0200

    removed unused code

--- vue.config.js
+++ vue.config.js
@@ -10,9 +10,6 @@ module.exports = defineConfig({
           ignored: /node_modules/,
           poll: 1000,
       },
-      publicPath: process.env.NODE_ENV === 'production'
-    ? '/siemens-submission/'
-    : '/'
   },
   transpileDependencies: true,
 })
